5c45f2842138e6e84b3f20d5db63c271aa69df4b
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-16 22:59:02 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BusinessCard></BusinessCard>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import BusinessCard from '../components/BusinessCard'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    BusinessCard,
   }
 }
 </script>
