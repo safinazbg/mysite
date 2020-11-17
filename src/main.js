7e0cf3c0a3855786e2906cd5c311947025e24350
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-17 19:11:38 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Papers from "./components/Papers/Papers";
 
 
 import 'bulma/bulma.sass'
@@ -9,4 +10,6 @@ import 'bulma/bulma.sass'
 createApp(App)
   .use(store)
   .use(router)
+  .component("Papers", Papers)
   .mount('#app')
+
