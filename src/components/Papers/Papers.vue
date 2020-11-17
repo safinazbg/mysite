05a4aff620af8d52db3139ee2b06f4d52131c5fd
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-17 19:13:09 +0100

    create papers component

---
+++ src/components/Papers/Papers.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="research">research</div>
+  <PaperCard
+      v-for="(paper, index) in papers"
+      :key="index"
+      :paper="paper"
+  ></PaperCard>
+</template>
+
+<script>
+  import PaperCard from "./PaperCard";
+  export default {
+    name: "Papers",
+    components:{
+      PaperCard,
+    },
+    data(){
+      return {
+        papers:[
+          {
+            title: "paper1",
+            autor: "Safinaz Buyukguzel",
+            content: "lorem ipsum dolor sit amet",
+            keywords: ["scrum meetings", "ethnomethodology", "conversation analysis","calculations"]
+          },
+          {
+            title: "paper2",
+            author: "Safinaz Buyukguzel",
+            content: "lorem ipsum...",
+            keywords: ["hybrid meetings", "daily scrum", "video-mediated collaboration"]
+          },
+          {
+            title: "paper3",
+            author: "Safinaz Buyukguzel",
+            content: "lorem ipsum....",
+            keywords:["scrum meetings", "reporting", "round-robin"]
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
