da6f83ce5095e0801f5a6e650cb5c6211023e2cf
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-20 10:07:28 +0100

    import ProExperiences and ProExperiencesCard components

--- src/main.js
+++ src/main.js
@@ -9,11 +9,15 @@ import '../node_modules/bulma/css/bulma.css'
 
 import 'bulma/bulma.sass'
 import Activities from "./components/Activities";
+import ProExperienceCard from "./components/ProfessionalExperience/ProExperienceCard";
+import ProExperiences from "./components/ProfessionalExperience/ProExperiences";
 
 createApp(App)
   .use(store)
   .use(router)
   .component("Papers", Papers)
   .component("Activities", Activities)
+  .component("ProExperienceCard", ProExperienceCard)
+  .component("ProExperiences", ProExperiences)
   .mount('#app')
 
