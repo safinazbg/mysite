238a7fe2494bb4fdaca308a83e892b35f35cd273
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-20 07:50:35 +0100

    create Professional Experience component

---
+++ src/components/ProfessionalExperience/ProExperienceCard.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="proExperienceCard">
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: "ProExperienceCard"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProfessionalExperience/ProExperiences.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="proExperiences">
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: "ProExperiences"
+  }
+</script>
+
+<style scoped>
+
+</style>
