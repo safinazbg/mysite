fe49cdb3a123a552aa8ece4c7ac0ea502ae1b955
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-20 07:50:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Papers from "./components/Papers/Papers";
+import '../node_modules/bulma/css/bulma.css'
+
 
 
 import 'bulma/bulma.sass'
+import Activities from "./components/Activities";
 
 createApp(App)
   .use(store)
   .use(router)
   .component("Papers", Papers)
+  .component("Activities", Activities)
   .mount('#app')
 
