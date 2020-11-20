eb78a5e2db0669ebe29cf345792c796f55aee761
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-20 10:06:28 +0100

    Update ProExperienceCard.vue

--- src/components/ProfessionalExperience/ProExperienceCard.vue
+++ src/components/ProfessionalExperience/ProExperienceCard.vue
@@ -1,12 +1,36 @@
 <template>
 <div class="proExperienceCard">
+  <div class="box">
+    <article class="media">
+      <button class="button is-white">{{experience.date}}</button>
+      <figure class="media-left">
+        <p class="image is-64x64">
+          <img src="{{experience.image}}"/>
 
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content has-text-left">
+          <p>
+            <strong>{{experience.position}}</strong> <a>{{experience.affiliation}}</a>, Dept. of Design and Communication
+          </p>
+          {{experience.description}}
+        </div>
+      </div>
+    </article>
+  </div>
 </div>
 </template>
 
 <script>
   export default {
-    name: "ProExperienceCard"
+    name: "ProExperienceCard",
+    props: {
+      experience: {
+        type: Object,
+        required: true,
+      }
+    }
   }
 </script>
 
