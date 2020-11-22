6e777454a02a37486530373bbdfda596f87404bf
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-22 21:41:56 +0100

    Create TalksAndPresentations.vue

---
+++ src/components/TalksAndPresentations.vue
@@ -0,0 +1,82 @@
+<template>
+<div class="talksAndPresentations">
+
+</div>
+</template>
+
+<script>
+  export default {
+    name: "TalksAndPresentations",
+    data(){
+      return{
+        talks: [
+          {
+            speakers: 'Buyukguzel, S, Wagner, J.',
+            title: 'Interactional Accomplishment of Calculations in Scrum Meetings',
+            event: 'VALS ASLA Conference',
+            location: 'University of Basel, Switzerland',
+            date: 'June 6th-8th 2018'
+          }, {
+            speakers: 'Sert, O, Balaman, U., Büyükgüzel, S.',
+            title: 'On the role of extra-mural English in Turkey: Young learners and teacher education',
+            event: 'GLOBELT2017 Conference',
+            location: 'İzmir, Turkey',
+            date: 'April 18th-21st 2017'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Political news interviews',
+            event: 'HUMAN Research Day',
+            location: 'Ankara',
+            date: 'March 10th 2017'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Avoidance as an interactional practice in political news interviews.',
+            event: 'The 6th International Conference on Applied Linguistics and Professional Practice (ALAPP)',
+            location: 'Copenhagen, Denmark',
+            date: 'November 3rd-5th 2016'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Avoidance strategies in political interviews',
+            event: 'The American Association for Applied Linguistics (AAAL) Conference',
+            location: 'Orlando, USA',
+            date: 'April 9th-12th 2016'
+          }, {
+            speakers: 'Gül, S., Büyükgüzel, S.',
+            title: 'Overlap Resolution in Media Talk: A Case Study from a Political Debate',
+            event: 'HUMAN Social Interaction and Applied Linguistics Postgraduate Conference',
+            location: 'Ankara',
+            date: 'September 8th 2015'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Les stratégies d’évitement dans l’interaction politique',
+            event: 'invited talk',
+            location: 'University of Lausanne, Switzerland',
+            date: 'May 21st 2015'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'La généralisation comme stratégie d’évitement dans l’interaction politique',
+            event: 'The American Association for Applied Linguistics (AAAL) Conference',
+            location: 'Toronto, Canada',
+            date: 'March 21st-24th 2015'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Politik Söylemde Konumlanma ve Tutum',
+            event: '28. Ulusal Dilbilim Kurultayı',
+            location: 'Sakarya Üniversitesi, Sakarya, Turkey',
+            date: 'May 8th-9th 2014'
+          }, {
+            speakers: 'Büyükgüzel, S.',
+            title: 'Gerekçelendirmeli Söylem Çözümlemesi: Fransız Kalmak',
+            event: '27. Ulusal Dilbilim Kurultayı',
+            date: 'May 2nd-4th 2013',
+            location: 'Antalya, Turkey'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
