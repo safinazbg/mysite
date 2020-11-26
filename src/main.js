c8af93410c5f07fdcf2b2fbc9b35aab35153a923
safinazbg <safinazbuyukguzel@gmail.com>
2020-11-26 09:20:23 +0100

    install Bulma timeline extension

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Papers from "./components/Papers/Papers";
+
 import '../node_modules/bulma/css/bulma.css'
+import '../node_modules/bulma-extensions/bulma-timeline/dist/css/bulma-timeline.min.css'
 
-
-
-import 'bulma/bulma.sass'
 import Activities from "./components/Activities";
 import ProExperienceCard from "./components/ProfessionalExperience/ProExperienceCard";
 import ProExperiences from "./components/ProfessionalExperience/ProExperiences";
+import WorkTimeline from "./components/ProfessionalExperience/WorkTimeline";
 
 createApp(App)
   .use(store)
@@ -19,5 +19,6 @@ createApp(App)
   .component("Activities", Activities)
   .component("ProExperienceCard", ProExperienceCard)
   .component("ProExperiences", ProExperiences)
+  .component("WorkTimeline", WorkTimeline )
   .mount('#app')
 
