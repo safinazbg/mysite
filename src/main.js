92fecaf224df4c0a91e56c573b77d22c186caea2
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-18 19:23:16 +0100

    imported uikit deleted bulma

--- src/main.js
+++ src/main.js
@@ -2,23 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Papers from "./components/Papers/Papers";
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
+import 'uikit/dist/css/uikit.css'
 
-import '../node_modules/bulma/css/bulma.css'
-import '../node_modules/bulma-extensions/bulma-timeline/dist/css/bulma-timeline.min.css'
+UIkit.use(Icons)
 
-import Activities from "./components/Activities";
-import ProExperienceCard from "./components/ProfessionalExperience/ProExperienceCard";
-import ProExperiences from "./components/ProfessionalExperience/ProExperiences";
-import WorkTimeline from "./components/ProfessionalExperience/WorkTimeline";
 
 createApp(App)
   .use(store)
   .use(router)
-  .component("Papers", Papers)
-  .component("Activities", Activities)
-  .component("ProExperienceCard", ProExperienceCard)
-  .component("ProExperiences", ProExperiences)
-  .component("WorkTimeline", WorkTimeline )
   .mount('#app')
 
