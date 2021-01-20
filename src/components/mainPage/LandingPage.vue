39351713205d49aeb20ac4c8b8cf089b2dff6d0d
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-20 17:04:06 +0100

    Create LandingPage.vue

---
+++ src/components/mainPage/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="uk-flex-center">
+  <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-card-media-left uk-cover-container">
+      <img src="../../assets/alone.png" uk-cover/>
+      <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+      <div class="uk-card-body">
+        <h3 class="uk-card-title">Research</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+    <div class="uk-flex-last@s uk-card-media-right uk-cover-container">
+      <img src="../../assets/together.png" uk-cover/>
+      <canvas width="600" height="400"></canvas>
+    </div>
+    <div>
+      <div class="uk-card-body">
+        <h3 class="uk-card-title">Professional Experiences</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name: "LandingPage"
+}
+</script>
+
+<style scoped>
+
+</style>
