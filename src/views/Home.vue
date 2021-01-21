650fd160af59aff1c75349591a8d626a9a1dff67
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-21 16:40:02 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-  <div class="container">
+  <div id="home" class="uk-container uk-width-1-1@s uk-flex-center">
+    <AboutMe></AboutMe>
     <LandingPage></LandingPage>
-  </div>
   </div>
 </template>
 
 <script>
+import AboutMe from "@/components/mainPage/AboutMe";
 import LandingPage from "@/components/mainPage/LandingPage";
 export default {
   name: 'Home',
   components: {
-    LandingPage
+    LandingPage, AboutMe
   }
 }
 </script>
 
+<style>
+#home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
