614a253496a44484d7e1712011ee9aa82613b427
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-21 22:57:39 +0100

    update typewriter text class details

--- src/components/mainPage/AboutMe.vue
+++ src/components/mainPage/AboutMe.vue
@@ -3,15 +3,15 @@
   <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
     <div id="me" class="uk-flex-last@s uk-card-media-right">
       <canvas width="600" height="500"></canvas>
-      <div class="uk-overlay uk-light uk-position-bottom-right">
-        <p class="subtitle">{{ displayedLine }}</p>
+      <div class="subtitle uk-overlay uk-light uk-position-bottom-right uk-padding-remove-right">
+        <p>{{ displayedLine }}</p>
       </div>
     </div>
     <div>
       <div class="uk-card-body">
         <h3 class="uk-card-title">About Me</h3>
         <p>I am a researcher, user experience designer, and front-end developer. I research the ways in which software development teams coordinate and collaborate in face to face and hybrid settings. I design and develop user friendly web applications. I am currently about to finish my second PhD at the <a href="https://www.sdu.dk/"> University of Southern Denmark </a> in Design, IT, and Communication. </p>
-       
+
         <ul class="uk-iconnav">
           <li><a href="#" uk-icon="icon: linkedin"></a></li>
           <li><a href="#" uk-icon="icon: github"></a></li>
@@ -84,7 +84,10 @@ name: "AboutMe",
   background-position-y: -23px;
 }
 .subtitle {
-  width: 100%;
+  text-align: center;
+  width: 50%;
+}
+.subtitle p {
   font-size: 105%;
   color: #46feb7;
   font-weight: 800;
