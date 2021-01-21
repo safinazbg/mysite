d4b3436ea7856a7f3330cb6ccb5f40ffcab51fc8
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-21 16:33:54 +0100

    delete old style

--- src/App.vue
+++ src/App.vue
@@ -46,36 +46,3 @@ em {
  }
 </style>
 
-<!--<style>-->
-<!--  @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');-->
-<!--  #app {-->
-<!--    font-family: Avenir, Helvetica, Arial, sans-serif;-->
-<!--    -webkit-font-smoothing: antialiased;-->
-<!--    -moz-osx-font-smoothing: grayscale;-->
-<!--    text-align: center;-->
-<!--    color: #2c3e50;-->
-<!--  }-->
-
-<!--  #nav {-->
-<!--    padding: 30px;-->
-<!--  }-->
-
-<!--  #nav a {-->
-<!--    font-weight: bold;-->
-<!--    color: #2c3e50;-->
-<!--  }-->
-
-<!--  #nav a.router-link-exact-active {-->
-<!--    color: #42b983;-->
-<!--  }-->
-
-
-<!--  p {-->
-<!--    font-family:noto sans jp, sans-serif ;-->
-
-<!--  }-->
-<!--  .main.title {-->
-<!--    font-family: 'VT323';-->
-<!--  }-->
-
-<!--</style>-->
