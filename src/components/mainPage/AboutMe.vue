9c2b191b005bc9914157fdfaadff01c6758e3ed1
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-21 22:25:13 +0100

    create typewriter animation

--- src/components/mainPage/AboutMe.vue
+++ src/components/mainPage/AboutMe.vue
@@ -17,6 +17,53 @@
 <script>
 export default {
 name: "AboutMe"
+  data () {
+    return {
+      displayedLine: '',
+      lineBeingWritten: -1,
+      lines: [
+          "Researcher",
+          "User Experience Designer",
+          "Front-end Developer"
+      ],
+      lineTimeout: null,
+      typingTimeout: null,
+    }
+  },
+  mounted() {
+    this.typeNextLine()
+  },
+  methods: {
+    typeLines() {
+      if (this.timeToNextLine)  this.timeToNextLine = clearTimeout(this.timeToNextLine)
+      this.timeToNextLine = setTimeout(this.typeNextLine, 2000)
+    },
+    typeNextLine() {
+      this.lineBeingWritten += 1
+      if (this.lineBeingWritten >= this.lines.length) this.lineBeingWritten = 0
+      this.displayedLine = ''
+      this.typeNextChar()
+    },
+    typeNextChar() {
+      const line = this.lines[this.lineBeingWritten]
+      const charToWrite = line[this.displayedLine.length]
+      const done = !charToWrite
+      if (done) {
+        this.typeLines()
+      } else {
+        const duration = Math.random() * 500
+        if (this.typingTimeout) this.typingTimeout = clearTimeout(this.typingTimeout)
+        this.typingTimeout = setTimeout(this.typeCharacter, duration, charToWrite)
+      }
+    },
+    typeCharacter (char) {
+      this.displayedLine += char
+      this.typeNextChar()
+    },
+    deleteChar () {
+      this.displayedLine = this.displayedLine.substring(0, this.displayedLine.length - 2)
+    }
+  }
 }
 </script>
 
