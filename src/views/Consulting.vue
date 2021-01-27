ac1b0eb1a33d60771b801f83c146c3a83d88ba99
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-27 19:34:38 +0100

    Update Consulting.vue

--- src/views/Consulting.vue
+++ src/views/Consulting.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="consulting">
     <p>hello</p>
-    <business-card></business-card>
   </div>
 </template>
 
 <script>
-  import BusinessCard from "../components/BusinessCard";
   export default {
     name: "Consulting",
     components: {
-      BusinessCard
     }
   }
 </script>
