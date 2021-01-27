960c76b1fc7b626a76a95d77514ade7723a57bc8
safinazbg <safinazbuyukguzel@gmail.com>
2021-01-27 16:30:22 +0100

    Update TimeLine.vue

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,9 +1,34 @@
 <template>
+  <div class="timeline">
+    <TimelinePoint
+        v-for="(item, index) in points"
+        :key="index"
+        :isEven="index % 2 === 0"
+        :point="item"
+    ></TimelinePoint>
   </div>
 </template>
 
 <script>
+  import TimelinePoint from "@/components/TimelinePoint";
   export default {
+    name: "TimeLine",
+    components: {
+      TimelinePoint
+    },
+    data() {
+      return {
+        points: [
+          {
+            date: '2017-present',
+            details: 'PhD Student at uni'
+          }, {
+            date: 'date',
+            details: 'other thingses'
+          }
+        ]
+      }
+    }
   }
 </script>
 
