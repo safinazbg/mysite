2ffa5bcd90e19f5cbbf1ebceccee3a8a0dcfa960
safinazbg <safinazbuyukguzel@gmail.com>
2021-02-07 16:23:21 +0100

    update activities wiith uikit card

--- src/components/Activities.vue
+++ src/components/Activities.vue
@@ -1,36 +1,18 @@
 <template>
 <div class="activities">
-
-  <div class="tile is-ancestor">
-    <div class="tile is-4 is-vertical is-parent">
-      <div class="tile is-child box">
-        <p class="title">Internship</p>
-        <p class="subtitle"> Microsoft Research Cambridge, UK</p>
-        <p>
-          In spring 2018 I had a amazing internship with Sean Rintel et al at Microsoft Research in Cambridge in the Human Experience
-          and Design group. My stay culminated in a video about the new Microsoft Teams features presented on
-          <a href="https://www.microsoft.com/en-us/garage/wall-of-fame/companionexperiences/">
-            this page
-          </a>.
-          I also got into the
-          <a href="https://www.nytimes.com/2018/07/03/technology/cambridge-artificial-intelligence.html">
-            focus of the New York Times
-          </a>
-          during my stay there.
-        </p>
-      </div>
-      <div class="tile is-child box">
-        <p class="title">Visiting Researcher</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+  <div class="uk-card uk-card-default uk-width-1-2@m">
+    <div class="uk-card-header">
+      <div class="uk-grid-small uk-flex-middle" uk-grid>
+        <div class="uk-width-auto">
+          <img class="uk-border-circle" width="40" height="40" src="../../public/avatars/sdu_logo.png" >
+        </div>
+        <div class="uk-width-expand">
+          <h5 class="uk-margin-remove-bottom">Title</h5>
+        </div>
       </div>
     </div>
-    <div class="tile is-parent">
-      <div class="tile is-child box">
-        <p class="title">Talks</p>
-        <p>Calculating as an embodied practice in scrum meetings Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-        <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-        <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
-      </div>
+    <div class="uk-card-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
     </div>
   </div>
 
